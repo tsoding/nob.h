9abee164684b9a16e485602da730932478f1d79a
rexim <reximkut@gmail.com>
2024-10-15 07:18:12 +0700

    [tests/minimal_log_level] State that the messages are test messages

--- tests/minimal_log_level.c
+++ tests/minimal_log_level.c
@@ -1,21 +1,21 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
 
-void log_some_messages(void)
+void log_test_messages(void)
 {
-    nob_log(NOB_INFO, "Info Message");
-    nob_log(NOB_WARNING, "Warning Message");
-    nob_log(NOB_ERROR, "Error Message");
+    nob_log(NOB_INFO, "Info Test Message");
+    nob_log(NOB_WARNING, "Warning Test Message");
+    nob_log(NOB_ERROR, "Error Test Message");
 }
 
 int main(void)
 {
-    log_some_messages();
+    log_test_messages();
     nob_minimal_log_level = NOB_WARNING;
-    log_some_messages();
+    log_test_messages();
     nob_minimal_log_level = NOB_ERROR;
-    log_some_messages();
+    log_test_messages();
     nob_minimal_log_level = NOB_NO_LOGS;
-    log_some_messages();
+    log_test_messages();
     return 0;
 }
