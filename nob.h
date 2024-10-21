69eda6c2eba7af670e43d29c0c0b0330d1f972ad
rexim <reximkut@gmail.com>
2024-10-21 17:34:55 +0700

    Release v1.3.3
    - Fix UX issues with NOB_GO_REBUILD_URSELF on Windows when you call nob without the .exe extension

--- nob.h
+++ nob.h
@@ -1,4 +1,4 @@
-/* nob - v1.3.2 - Public Domain - https://github.com/tsoding/nob
+/* nob - v1.3.3 - Public Domain - https://github.com/tsoding/nob
 
    This library is the next generation of the [NoBuild](https://github.com/tsoding/nobuild) idea.
 
@@ -1326,7 +1326,8 @@ int closedir(DIR *dirp)
 /*
    Revision history:
 
-      1.3.2 (2024-10.21) Fix unreachable error in nob_log on passing NOB_NO_LOGS
+      1.3.3 (2024-10-21) Fix UX issues with NOB_GO_REBUILD_URSELF on Windows when you call nob without the .exe extension
+      1.3.2 (2024-10-21) Fix unreachable error in nob_log on passing NOB_NO_LOGS
       1.3.1 (2024-10-21) Fix redeclaration error for minimal_log_level (By @KillerxDBr)
       1.3.0 (2024-10-17) Add NOB_UNREACHABLE
       1.2.2 (2024-10-16) Fix compilation of nob_cmd_run_sync_and_reset on Windows (By @KillerxDBr)
