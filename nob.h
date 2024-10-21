268efd1103d66738ed7b80db25d7a1f95d22bc7f
Antonio Roberto <antonioroberto1407@gmail.com>
2024-10-21 19:53:26 -0400

    fixing format string, removing unused variable

--- nob.h
+++ nob.h
@@ -432,13 +432,13 @@ char *nob_log_win32_error(DWORD err) {
 
     if (errMsgSize == 0) {
         if (GetLastError() != ERROR_MR_MID_NOT_FOUND) {
-            if (sprintf(win32ErrMsg, "Error while getting message error for 0x%0X", err) > 0) {
+            if (sprintf(win32ErrMsg, "Error while getting message error for 0x%08lX", err) > 0) {
                 return (char *)&win32ErrMsg;
             } else {
                 return NULL;
             }
         } else {
-            if (sprintf(win32ErrMsg, "Invalid Windows Error code (0x%08X)", err) > 0) {
+            if (sprintf(win32ErrMsg, "Invalid Windows Error code (0x%08lX)", err) > 0) {
                 return (char *)&win32ErrMsg;
             } else {
                 return NULL;
@@ -446,7 +446,7 @@ char *nob_log_win32_error(DWORD err) {
         }
     }
 
-    DWORD bkpSize = errMsgSize;
+    // DWORD bkpSize = errMsgSize;
 
     // TODO: check if last 2 chars are actualy CRLF
     // removing line breaks
