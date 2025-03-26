a8820f073f8e5ee8982032eeda9b598cc29f56c8
KillerxDBr <97485370+KillerxDBr@users.noreply.github.com>
2025-03-26 18:40:40 -0400

    Fix for mingw NOB_PRINTF_FORMAT
    To avoid Warnings when using %zu with printf style nob functions

--- nob.h
+++ nob.h
@@ -213,7 +213,11 @@
 
 #if defined(__GNUC__) || defined(__clang__)
 // https://gcc.gnu.org/onlinedocs/gcc-4.7.2/gcc/Function-Attributes.html
+#ifdef __MINGW32__
+#define NOB_PRINTF_FORMAT(STRING_INDEX, FIRST_TO_CHECK) __attribute__ ((format (__MINGW_PRINTF_FORMAT, STRING_INDEX, FIRST_TO_CHECK)))
+#else
 #define NOB_PRINTF_FORMAT(STRING_INDEX, FIRST_TO_CHECK) __attribute__ ((format (printf, STRING_INDEX, FIRST_TO_CHECK)))
+#endif
 #else
 // TODO: implement NOB_PRINTF_FORMAT for MSVC
 #define NOB_PRINTF_FORMAT(STRING_INDEX, FIRST_TO_CHECK)
