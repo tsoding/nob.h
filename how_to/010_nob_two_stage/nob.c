7af5023f4e41cb30554bc43740861480517b6978
rexim <reximkut@gmail.com>
2025-04-16 15:28:21 +0700

    [v1.20.0] Introduce the nob_cc_* macros

--- how_to/010_nob_two_stage/nob.c
+++ how_to/010_nob_two_stage/nob.c
@@ -31,11 +31,11 @@ int main(int argc, char **argv)
 
     const char *output_path = BUILD_FOLDER"nob_configed";
     const char *input_path = SRC_BUILD_FOLDER"nob_configed.c";
-    cmd_append(&cmd,
-               "gcc",
-               "-Wall", "-Wextra", "-ggdb",
-               "-I.", "-I"BUILD_FOLDER, "-I"SRC_BUILD_FOLDER,
-               "-o", output_path, input_path);
+    nob_cc(&cmd);
+    nob_cc_flags(&cmd);
+    nob_cmd_append(&cmd, "-I.", "-I"BUILD_FOLDER, "-I"SRC_BUILD_FOLDER); // -I is usually the same across all compilers
+    nob_cc_output(&cmd, output_path);
+    nob_cc_inputs(&cmd, input_path);
     if (!cmd_run_sync_and_reset(&cmd)) return 1;
 
     cmd_append(&cmd, output_path);
