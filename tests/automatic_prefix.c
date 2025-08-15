720b3f82dd4babee4bb649acb0ac3a34162f48c1
GioseaxMC <gioseaxdiscord@gmail.com>
2025-08-15 23:21:59 +0200

    Made implementation test compliant

--- tests/automatic_prefix.c
+++ tests/automatic_prefix.c
@@ -6,8 +6,10 @@ void nob(print) (char* mes) {
     printf("> %s\n", mes);
 }
 
-void main() {
+int main() {
     nob(print)("This always compiles");
 
-    nob_print("This compiles only with NOB_STRIP_PREFIX defined");
+    nob_print("This compiles only without NOB_STRIP_PREFIX defined");
+
+    return 0;
 }
