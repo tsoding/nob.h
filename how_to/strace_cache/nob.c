b8e90c0ec8cb9fd93feb03e67929d026b48da7cb
Branimir Ri\v{c}ko <rickobranimir@gmail.com>
2025-08-31 13:02:33 +0200

    Typo.

--- how_to/strace_cache/nob.c
+++ how_to/strace_cache/nob.c
@@ -3,12 +3,12 @@
 
 int main(int argc, char **argv)
 {
-    NOB_GO_REBUILD_URSELF(argc, argv);
+    NOB_GO_REBUILD_URSELF_PLUS(argc, argv, "nob.h");
     if (!nob_mkdir_if_not_exists("build")) return 1;
 
     Nob_Cmd cmd = {0};
-    // Cache data will be read and witten to a .file_path field ("build/nob.cache" in this case.)
-    // In case .file_path filed is not set, cache data will only be in memory.
+    // Cache data will be read and written to a .file_path field ("build/nob.cache" in this case.)
+    // In case .file_path field is not set, cache data will only be in memory.
     Nob_Strace_Cache cache = { .file_path = "build/nob.cache" };
 
     for (int i = 0; i < 10; ++i) {
@@ -26,6 +26,8 @@ int main(int argc, char **argv)
     // Save cache data to disc.
     // And free all memory malloced by strace_cache.
     nob_strace_cache_finish(cache);
+
+    nob_cmd_free(cmd);
 
     return 0;
 }
