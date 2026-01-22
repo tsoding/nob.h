18aa9e92c0200d1d58f7db94f088fb0a9c705a52
rexim <reximkut@gmail.com>
2026-01-22 08:09:20 +0700

    Introduce record/replay testing mechanism

--- nob.c
+++ nob.c
@@ -30,7 +30,7 @@ const char *test_names[] = {
 };
 #define test_names_count ARRAY_LEN(test_names)
 
-bool build_and_run_test(Cmd *cmd, const char *test_name)
+bool build_and_run_test(Cmd *cmd, const char *test_name, bool record)
 {
     size_t mark = temp_save();
 
@@ -46,13 +46,75 @@ bool build_and_run_test(Cmd *cmd, const char *test_name)
     if (!mkdir_if_not_exists(test_cwd_path)) return false;
     if (!set_current_dir(test_cwd_path)) return false;
     cmd_append(cmd, temp_sprintf("../%s", test_name));
-    if (!cmd_run(cmd))    return false;
+    const char *test_stdout_path = temp_sprintf("../%s.stdout.txt", test_name);
+    const char *test_stderr_path = temp_sprintf("../%s.stderr.txt", test_name);
+    if (!cmd_run(cmd, .stdout_path = test_stdout_path, .stderr_path = test_stderr_path)) return false;
     if (!set_current_dir("../../../")) return false;
+
+    const char *src_stdout_path = temp_sprintf("%s%s.stdout.txt", BUILD_FOLDER TESTS_FOLDER, test_name);
+    const char *src_stderr_path = temp_sprintf("%s%s.stderr.txt", BUILD_FOLDER TESTS_FOLDER, test_name);
+    const char *dst_stdout_path = temp_sprintf("%s%s.stdout.txt", TESTS_FOLDER, test_name);
+    const char *dst_stderr_path = temp_sprintf("%s%s.stderr.txt", TESTS_FOLDER, test_name);
+    if (record) {
+        if (!copy_file(src_stdout_path, dst_stdout_path)) return 1;
+        if (!copy_file(src_stderr_path, dst_stderr_path)) return 1;
+    } else {
+        cmd_append(cmd, "diff");
+        cmd_append(cmd, "-u");
+        cmd_append(cmd, src_stdout_path);
+        cmd_append(cmd, dst_stdout_path);
+        if (!cmd_run(cmd)) return false;
+
+        cmd_append(cmd, "diff");
+        cmd_append(cmd, "-u");
+        cmd_append(cmd, src_stderr_path);
+        cmd_append(cmd, dst_stderr_path);
+        if (!cmd_run(cmd)) return false;
+    }
 
     nob_log(INFO, "--- %s finished ---", bin_path);
 
     temp_rewind(mark);
     return true;
+}
+
+typedef struct {
+    const char *name;
+    const char *signature;
+    const char *description;
+} Command;
+
+typedef struct {
+    Command *items;
+    size_t count;
+    size_t capacity;
+} Commands;
+
+bool command(const char *arg, Commands *commands, const char *name, const char *signature, const char *description)
+{
+    Command command = {
+        .name = name,
+        .description = description,
+        .signature = signature,
+    };
+    da_append(commands, command);
+    return strcmp(arg, name) == 0;
+}
+
+void print_available_commands(Commands commands)
+{
+    size_t max_name_width = 0;
+    size_t max_sign_width = 0;
+    da_foreach(Command, command, &commands) {
+        size_t name_width = strlen(command->name);
+        size_t sign_width = strlen(command->signature);
+        if (name_width > max_name_width) max_name_width = name_width;
+        if (sign_width > max_sign_width) max_sign_width = sign_width;
+    }
+    nob_log(INFO, "Available commands:");
+    da_foreach(Command, command, &commands) {
+        nob_log(INFO, "    %-*s %-*s - %s", (int)max_name_width, command->name, (int)max_sign_width, command->signature, command->description);
+    }
 }
 
 int main(int argc, char **argv)
@@ -67,25 +129,45 @@ int main(int argc, char **argv)
     const char *command_name = "test";
     if (argc > 0) command_name = shift(argv, argc);
 
-    if (!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
-    if (!mkdir_if_not_exists(BUILD_FOLDER TESTS_FOLDER)) return 1;
+    Commands commands = {0};
 
-    if (strcmp(command_name, "test") == 0) {
+    if (command(command_name, &commands, "test", "[test_names...]", "Run the tests checking their expected output")) {
+        if (!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+        if (!mkdir_if_not_exists(BUILD_FOLDER TESTS_FOLDER)) return 1;
+
         if (argc <= 0) {
             for (size_t i = 0; i < test_names_count; ++i) {
-                if (!build_and_run_test(&cmd, test_names[i])) return 1;
+                if (!build_and_run_test(&cmd, test_names[i], false)) return 1;
             }
             return 0;
         }
 
         while (argc > 0) {
             const char *test_name = shift(argv, argc);
-            if (!build_and_run_test(&cmd, test_name)) return 1;
+            if (!build_and_run_test(&cmd, test_name, false)) return 1;
         }
         return 0;
     }
 
-    if (strcmp(command_name, "list") == 0) {
+    if (command(command_name, &commands, "record", "[test_names...]", "Record expected output of the tests")) {
+        if (!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+        if (!mkdir_if_not_exists(BUILD_FOLDER TESTS_FOLDER)) return 1;
+
+        if (argc <= 0) {
+            for (size_t i = 0; i < test_names_count; ++i) {
+                if (!build_and_run_test(&cmd, test_names[i], true)) return 1;
+            }
+            return 0;
+        }
+
+        while (argc > 0) {
+            const char *test_name = shift(argv, argc);
+            if (!build_and_run_test(&cmd, test_name, true)) return 1;
+        }
+        return 0;
+    }
+
+    if (command(command_name, &commands, "list", "", "List available tests")) {
         nob_log(INFO, "Tests:");
         for (size_t i = 0; i < test_names_count; ++i) {
             nob_log(INFO, "    %s", test_names[i]);
@@ -94,6 +176,12 @@ int main(int argc, char **argv)
         return 0;
     }
 
+    if (command(command_name, &commands, "help", "", "Print this help message")) {
+        print_available_commands(commands);
+        return 0;
+    }
+
+    print_available_commands(commands);
     nob_log(ERROR, "Unknown command %s", command_name);
     return 1;
 }
