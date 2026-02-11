cd3778339210dff639b94ba2627e0b54acad9d1d
rexim <reximkut@gmail.com>
2026-02-11 16:10:02 +0700

    Add walk_dirs example

--- how_to/nob.c
+++ how_to/nob.c
@@ -1,34 +1,34 @@
 // Makes sure the examples are buildable. Used usually on CI.
 #include "../shared.h"
 
-const char *examples[] = {
-    "001_basic_usage",
-    "005_parallel_build",
-    "010_nob_two_stage",
-};
+bool visit(Nob_Walk_Entry entry)
+{
+    if (entry.type == FILE_DIRECTORY && entry.level) {
+        Cmd *cmd = entry.data;
+        size_t mark = temp_save();
+            if (!set_current_dir(entry.path)) return 1;
+                nob_cc(cmd);
+                nob_cc_flags(cmd);
+                nob_cc_output(cmd, "./nob");
+                nob_cc_inputs(cmd, "nob.c");
+                if (!cmd_run(cmd)) return 1;
+
+                cmd_append(cmd, "./nob");
+                if (!cmd_run(cmd)) return 1;
+            if (!set_current_dir("..")) return 1;
+        temp_rewind(mark);
+
+        *entry.action = WALK_SKIP;
+    }
+    return true;
+}
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
     Cmd cmd = {0};
-
-    for (size_t i = 0; i < ARRAY_LEN(examples); ++i) {
-        const char *example = examples[i];
-        nob_log(INFO, "--- %s ---", example);
-        size_t mark = temp_save();
-            if (!set_current_dir(temp_sprintf("./%s", example))) return 1;
-                nob_cc(&cmd);
-                nob_cc_flags(&cmd);
-                nob_cc_output(&cmd, "./nob");
-                nob_cc_inputs(&cmd, "nob.c");
-                if (!cmd_run(&cmd)) return 1;
-
-                cmd_append(&cmd, "./nob");
-                if (!cmd_run(&cmd)) return 1;
-            if (!set_current_dir("..")) return 1;
-        temp_rewind(mark);
-    }
+    if (!nob_walk_dir(".", visit, .data = &cmd)) return 1;
 
     return 0;
 }
