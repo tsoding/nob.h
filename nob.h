da145246881e10aed470d0e6d3f7e87086feb9a6
gegoxaren <58400667+gegoxaren@users.noreply.github.com>
2026-04-16 02:45:57 +0200

    [nob.h] undeclare NOB_IMPLEMENTATION
    Undefine implementation macro after passning the #ifdef to prevent certain types of include errors.
    
    If a header file that include nob.h is include after includation of nob.h that has an implementation, it would caused redefition errors.

--- nob.h
+++ nob.h
@@ -957,6 +957,7 @@ NOBDEF char *nob_win32_error_message(DWORD err);
 #endif // NOB_H_
 
 #ifdef NOB_IMPLEMENTATION
+#undef NOB_IMPLEMENTATION
 
 // This is like nob_proc_wait() but waits asynchronously. Depending on the platform ms means different thing.
 // On Windows it means timeout. On POSIX it means for how long to sleep after checking if the process exited,
